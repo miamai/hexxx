507e1febf1a2197f9e15f02c9aec35e06ef2e945
miamai <cb121466@gmail.com>
2022-11-01 18:26:09 +0800

    Update 2022-11-01T10:25:58.286Z

--- app/assets/js/all.js
+++
@@ -1,6 +0,0 @@
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".nav-header");
-
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("open");
-});
---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var btnNavEl = document.querySelector(".btn-mobile-nav");
+var headerEl = document.querySelector(".nav-header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("open");
+});
+//# sourceMappingURL=all.js.map
