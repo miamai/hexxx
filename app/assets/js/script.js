518e4d78e6409badf5fd7c8aa0ebccd210ab5aeb
miamai <cb121466@gmail.com>
2022-11-01 18:22:29 +0800

    second commit

--- app/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// Mobile navigation
-
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector("header");
-
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
